eb8a8c445944b9650299a73e82a90a70984dd55e
Nate Catelli <ncatelli@packetfire.org>
2022-04-06 01:43:43 +0000

    stub out character matches

--- relex/src/compiler.rs
+++ relex/src/compiler.rs
@@ -19,5 +19,49 @@ pub fn compile(regex_ast: ast::Regex) -> Result<Opcodes, String> {
 }
 
 fn expression(expr: ast::Expression) -> Result<Opcodes, String> {
-    todo!()
+    let ast::Expression(subexprs) = expr;
+
+    subexprs
+        .into_iter()
+        .map(subexpression)
+        .collect::<Result<Vec<Opcodes>, _>>()
+        .map(|opcodes| opcodes.into_iter().flatten().collect())
 }
+
+fn subexpression(subexpr: ast::SubExpression) -> Result<Opcodes, String> {
+    let ast::SubExpression(items) = subexpr;
+
+    items
+        .into_iter()
+        .map(|subexpr_item| match subexpr_item {
+            ast::SubExpressionItem::Match(m) => match_item(m),
+            ast::SubExpressionItem::Group(_) => todo!(),
+            ast::SubExpressionItem::Anchor(_) => todo!(),
+            ast::SubExpressionItem::Backreference(_) => todo!(),
+        })
+        .collect::<Result<Vec<Opcodes>, _>>()
+        .map(|opcodes| opcodes.into_iter().flatten().collect())
+}
+
+fn match_item(m: ast::Match) -> Result<Opcodes, String> {
+    use ast::{Char, Match, MatchCharacter, MatchItem};
+
+    match m {
+        Match::WithQuantifier {
+            item: _,
+            quantifier: _,
+        } => todo!(),
+        Match::WithoutQuantifier {
+            item: MatchItem::MatchAnyCharacter,
+        } => todo!(),
+        Match::WithoutQuantifier {
+            item: MatchItem::MatchCharacter(MatchCharacter(Char(c))),
+        } => Ok(vec![Opcode::Consume(InstConsume::new(c))]),
+        Match::WithoutQuantifier {
+            item: MatchItem::MatchCharacterClass(_mcc),
+        } => todo!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {}
