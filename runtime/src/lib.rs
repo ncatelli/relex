b036c2da4726b1b9445b92775d0c42aa2d8edbd3
Nate Catelli <ncatelli@packetfire.org>
2022-04-15 16:47:00 +0000

    use any to circuit break early

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -601,7 +601,7 @@ pub fn run<const SG: usize>(program: &[Instruction], input: &str) -> Option<Vec<
     }
 
     // Signifies all savegroups are satisfied
-    let all_complete = sub.iter().all(|sg| sg.is_complete());
+    let all_complete = sub.iter().any(|sg| sg.is_none());
     if matches > 0 && all_complete {
         Some(sub)
     } else {
