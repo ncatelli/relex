77dc44067d37e2705074d8dfdd5f0177d68b9fec
Nate Catelli <ncatelli@packetfire.org>
2022-04-19 20:25:27 +0000

    extract alternation generation into function

--- relex/src/compiler.rs
+++ relex/src/compiler.rs
@@ -109,65 +109,13 @@ pub fn compile(regex_ast: ast::Regex) -> Result<Instructions, String> {
 
 fn expression(expr: ast::Expression) -> Result<RelativeOpcodes, String> {
     let ast::Expression(subexprs) = expr;
-    let subexpr_cnt = subexprs.len();
 
     let compiled_subexprs = subexprs
         .into_iter()
         .map(subexpression)
         .collect::<Result<Vec<_>, _>>()?;
 
-    let length_of_each_subexpr: Vec<_> = compiled_subexprs
-        .iter()
-        .enumerate()
-        .map(|(idx, subexpr)| ((idx + 1 == subexpr_cnt), subexpr))
-        .map(|(is_last, subexpr)| {
-            // last alternation doesn't require a split prefix and jump suffix
-            if is_last {
-                subexpr.len()
-            } else {
-                subexpr.len() + 2
-            }
-        })
-        .collect();
-
-    let total_length_of_compiled_expr: usize = length_of_each_subexpr.iter().sum();
-    let start_end_offsets_by_subexpr: Vec<(usize, usize)> = length_of_each_subexpr
-        .iter()
-        .fold(
-            // add 1 to set end at first instruction of next expr
-            (total_length_of_compiled_expr + 1, vec![]),
-            |(offset_to_end, mut acc), &subexpr_len| {
-                let new_offset_to_end = offset_to_end - subexpr_len;
-
-                acc.push((subexpr_len, new_offset_to_end));
-                (new_offset_to_end, acc)
-            },
-        )
-        .1;
-
-    let compiled_subexpressions_with_applied_alternations = compiled_subexprs
-        .into_iter()
-        .zip(start_end_offsets_by_subexpr.into_iter())
-        .enumerate()
-        .map(|(idx, (opcodes, start_end_offsets))| {
-            let optional_next_offsets = ((idx + 1) != subexpr_cnt)
-                .then(|| start_end_offsets)
-                .map(|(start, end)| (start as isize, end as isize));
-            (optional_next_offsets, opcodes)
-        })
-        .flat_map(|(start_of_next, ops)| match start_of_next {
-            Some((start_of_next_subexpr_offset, end_of_expr_offset)) => {
-                [RelativeOpcode::Split(1, start_of_next_subexpr_offset)]
-                    .into_iter()
-                    .chain(ops.into_iter())
-                    .chain([RelativeOpcode::Jmp(end_of_expr_offset)].into_iter())
-                    .collect()
-            }
-            None => ops,
-        })
-        .collect();
-
-    Ok(compiled_subexpressions_with_applied_alternations)
+    alternations_for_supplied_relative_opcodes(compiled_subexprs)
 }
 
 fn subexpression(subexpr: ast::SubExpression) -> Result<RelativeOpcodes, String> {
@@ -571,6 +519,66 @@ fn character_class_to_set(cc: ast::CharacterClass) -> CharacterSet {
         ast::CharacterClass::AnyDecimalDigit => AnyDecimalDigitClass.into(),
         ast::CharacterClass::AnyDecimalDigitInverted => AnyDecimalDigitClassInverted.into(),
     }
+}
+
+/// Generates alternations from a block of relative operations.
+fn alternations_for_supplied_relative_opcodes(
+    rel_ops: Vec<RelativeOpcodes>,
+) -> Result<RelativeOpcodes, String> {
+    let subexpr_cnt = rel_ops.len();
+
+    let length_of_rel_ops: Vec<_> = rel_ops
+        .iter()
+        .enumerate()
+        .map(|(idx, subexpr)| ((idx + 1 == subexpr_cnt), subexpr))
+        .map(|(is_last, subexpr)| {
+            // last alternation doesn't require a split prefix and jump suffix
+            if is_last {
+                subexpr.len()
+            } else {
+                subexpr.len() + 2
+            }
+        })
+        .collect();
+
+    let total_length_of_compiled_expr: usize = length_of_rel_ops.iter().sum();
+    let start_end_offsets_by_subexpr: Vec<(usize, usize)> = length_of_rel_ops
+        .iter()
+        .fold(
+            // add 1 to set end at first instruction of next expr
+            (total_length_of_compiled_expr + 1, vec![]),
+            |(offset_to_end, mut acc), &subexpr_len| {
+                let new_offset_to_end = offset_to_end - subexpr_len;
+
+                acc.push((subexpr_len, new_offset_to_end));
+                (new_offset_to_end, acc)
+            },
+        )
+        .1;
+
+    let compiled_ops_with_applied_alternations = rel_ops
+        .into_iter()
+        .zip(start_end_offsets_by_subexpr.into_iter())
+        .enumerate()
+        .map(|(idx, (opcodes, start_end_offsets))| {
+            let optional_next_offsets = ((idx + 1) != subexpr_cnt)
+                .then(|| start_end_offsets)
+                .map(|(start, end)| (start as isize, end as isize));
+            (optional_next_offsets, opcodes)
+        })
+        .flat_map(|(start_of_next, ops)| match start_of_next {
+            Some((start_of_next_subexpr_offset, end_of_expr_offset)) => {
+                [RelativeOpcode::Split(1, start_of_next_subexpr_offset)]
+                    .into_iter()
+                    .chain(ops.into_iter())
+                    .chain([RelativeOpcode::Jmp(end_of_expr_offset)].into_iter())
+                    .collect()
+            }
+            None => ops,
+        })
+        .collect();
+
+    Ok(compiled_ops_with_applied_alternations)
 }
 
 #[cfg(test)]
