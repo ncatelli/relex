0ad23fac696a658c38074f6cfd9abc31d4358b01
Nate Catelli <ncatelli@packetfire.org>
2022-05-06 01:10:23 +0000

    stub parser for relex

--- relex/src/ast.rs
+++ relex/src/ast.rs
@@ -1,19 +1,48 @@
+use std::fmt::Write;
+
 #[derive(Debug, PartialEq)]
-pub struct Rules(Vec<Rule>);
+pub struct Rules(pub Vec<Rule>);
 
 #[derive(Debug, PartialEq)]
 pub struct Rule {
-    name: Name,
+    identifier: Identifier,
     capture_type: Option<CaptureType>,
     pattern: Pattern,
     action: Action,
 }
 
-#[derive(Debug, PartialEq)]
-pub struct Name(Identifier);
+impl Rule {
+    pub fn new(
+        identifier: Identifier,
+        capture_type: Option<CaptureType>,
+        pattern: Pattern,
+        action: Action,
+    ) -> Self {
+        Self {
+            identifier,
+            capture_type,
+            pattern,
+            action,
+        }
+    }
+}
 
 #[derive(Debug, PartialEq)]
-pub struct CaptureType(Vec<CaptureTypeItem>);
+pub struct Identifier(pub String);
+
+impl Identifier {
+    pub fn try_new(id: String) -> Option<Self> {
+        let is_valid = id.chars().all(|c| c.is_alphabetic() || c == '_');
+        if is_valid {
+            Some(Identifier(id))
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct CaptureType(pub Vec<CaptureTypeItem>);
 
 #[derive(Debug, PartialEq)]
 pub enum CaptureTypeItem {
@@ -24,8 +53,14 @@ pub enum CaptureTypeItem {
 
 #[derive(Debug, PartialEq)]
 pub struct IntType {
-    sign: IntTypeSign,
-    width: IntTypeBitWidth,
+    pub sign: IntTypeSign,
+    pub width: IntTypeBitWidth,
+}
+
+impl IntType {
+    pub fn new(sign: IntTypeSign, width: IntTypeBitWidth) -> Self {
+        Self { sign, width }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -43,33 +78,63 @@ pub enum IntTypeBitWidth {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Pattern(PatternItem);
+pub struct Pattern(pub PatternItem);
 
-#[derive(Debug, PartialEq)]
-pub struct PatternItem(Vec<Char>);
-
-#[derive(Debug, PartialEq)]
-pub struct Action(ActionItem);
-
-#[derive(Debug, PartialEq)]
-pub struct ActionItem(Vec<Char>);
-
-#[derive(Debug, PartialEq)]
-pub struct Identifier(String);
-
-impl Identifier {
-    pub fn try_new(id: String) -> Option<Self> {
-        let is_valid = id.chars().all(|c| c.is_alphabetic() || c == '_');
-        if is_valid {
-            Some(Identifier(id))
-        } else {
-            None
-        }
+impl From<Pattern> for String {
+    fn from(src: Pattern) -> Self {
+        src.0.to_string()
     }
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Char(char);
+pub struct PatternItem(pub Vec<Char>);
+
+impl From<PatternItem> for String {
+    fn from(src: PatternItem) -> Self {
+        src.to_string()
+    }
+}
+
+impl std::fmt::Display for PatternItem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for c in self.0.iter().map(|c| c.as_char()) {
+            f.write_char(c)?
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Action(pub ActionItem);
+
+impl From<Action> for String {
+    fn from(src: Action) -> Self {
+        src.0.to_string()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ActionItem(pub Vec<Char>);
+
+impl From<ActionItem> for String {
+    fn from(src: ActionItem) -> Self {
+        src.to_string()
+    }
+}
+
+impl std::fmt::Display for ActionItem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for c in self.0.iter().map(|c| c.as_char()) {
+            f.write_char(c)?
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Char(pub char);
 
 impl Char {
     pub fn as_char(&self) -> char {
---
+++ relex/src/parser.rs
@@ -0,0 +1,152 @@
+use parcel::{one_or_more, prelude::v1::*};
+
+use super::ast;
+
+#[derive(PartialEq)]
+pub enum ParseErr {
+    InvalidRule,
+    Undefined(String),
+}
+
+impl std::fmt::Debug for ParseErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Undefined(err) => write!(f, "undefined parse error: {}", err),
+            Self::InvalidRule => write!(f, "provided rule is invalid",),
+        }
+    }
+}
+
+pub fn parse(_input: &[(usize, char)]) -> Result<ast::Rules, ParseErr> {
+    todo!()
+}
+
+fn rules<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::Rules> {
+    parcel::one_or_more(rule()).map(ast::Rules)
+}
+
+fn rule<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::Rule> {
+    use parcel::parsers::character::expect_str;
+
+    parcel::join(
+        identifier(),
+        parcel::join(
+            parcel::optional(capture_type()),
+            parcel::join(
+                pattern(),
+                parcel::right(parcel::join(expect_str("=>"), action())),
+            ),
+        ),
+    )
+    .map(|(identifier, (capture_type, (pattern, action)))| {
+        ast::Rule::new(identifier, capture_type, pattern, action)
+    })
+}
+
+fn identifier<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::Identifier> {
+    use parcel::parsers::character;
+
+    parcel::join(
+        character::alphabetic().predicate(|c| c.is_ascii_uppercase()),
+        parcel::zero_or_more(parcel::or(character::alphabetic(), || {
+            character::expect_character('_')
+        })),
+    )
+    .map(|(head, tail)| [head].into_iter().chain(tail.into_iter()).collect())
+    .map(ast::Identifier)
+}
+
+fn capture_type<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::CaptureType> {
+    use parcel::parsers::character;
+    parcel::right(parcel::join(
+        character::expect_character('['),
+        parcel::left(parcel::join(
+            parcel::one_or_more(capture_type_item()),
+            character::expect_character(']'),
+        )),
+    ))
+    .map(ast::CaptureType)
+}
+
+fn capture_type_item<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::CaptureTypeItem> {
+    use parcel::parsers::character;
+
+    parcel::or(
+        character::expect_str("String").map(|_| ast::CaptureTypeItem::String),
+        || {
+            parcel::or(int_type().map(ast::CaptureTypeItem::Int), || {
+                character::expect_str("bool").map(|_| ast::CaptureTypeItem::Bool)
+            })
+        },
+    )
+}
+
+fn int_type<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::IntType> {
+    parcel::join(int_type_sign(), int_type_bit_width())
+        .map(|(sign, width)| ast::IntType::new(sign, width))
+}
+
+fn int_type_sign<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::IntTypeSign> {
+    use parcel::parsers::character::expect_character;
+
+    parcel::or(
+        expect_character('i').map(|_| ast::IntTypeSign::Signed),
+        || expect_character('u').map(|_| ast::IntTypeSign::Unsigned),
+    )
+}
+
+fn int_type_bit_width<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::IntTypeBitWidth> {
+    use parcel::parsers::character::expect_str;
+
+    parcel::or(expect_str("8").map(|_| ast::IntTypeBitWidth::Eight), || {
+        parcel::or(
+            expect_str("16").map(|_| ast::IntTypeBitWidth::Sixteen),
+            || {
+                parcel::or(
+                    expect_str("32").map(|_| ast::IntTypeBitWidth::ThirtyTwo),
+                    || expect_str("64").map(|_| ast::IntTypeBitWidth::SixtyFour),
+                )
+            },
+        )
+    })
+}
+
+fn pattern<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::Pattern> {
+    use parcel::parsers::character;
+
+    parcel::right(parcel::join(
+        character::expect_character('['),
+        parcel::left(parcel::join(
+            pattern_item(),
+            character::expect_character(']'),
+        )),
+    ))
+    .map(ast::Pattern)
+}
+
+fn pattern_item<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::PatternItem> {
+    one_or_more(char()).map(ast::PatternItem)
+}
+
+fn action<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::Action> {
+    use parcel::parsers::character;
+
+    parcel::right(parcel::join(
+        character::expect_character('{'),
+        parcel::left(parcel::join(
+            action_item(),
+            character::expect_character('}'),
+        )),
+    ))
+    .map(ast::Action)
+}
+
+fn action_item<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::ActionItem> {
+    one_or_more(char()).map(ast::ActionItem)
+}
+
+fn char<'a>() -> impl parcel::Parser<'a, &'a [(usize, char)], ast::Char> {
+    use parcel::parsers::character;
+
+    character::any_character().map(ast::Char)
+}
